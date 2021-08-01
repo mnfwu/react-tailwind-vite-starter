fb480b8cd09834389c2e490f5f1ca3abadebaea4
Matthew Wu <mnfwu12@gmail.com>
2021-08-01 22:23:41 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           </button>
         </p>
         <p className="font-bold">
-          Edit <code>App.jsx</code> and save to test HMR updates.
+          Edit <code>App.jsx</code> and save to test HMR updates
         </p>
         <p>
           <a
